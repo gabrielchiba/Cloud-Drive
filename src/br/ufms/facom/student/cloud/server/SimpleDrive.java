9425c549ea212b212acf931e9de0c18fd40fe116
Felipe Silva <felipemsvl@gmail.com>
2020-06-29 17:18:22 -0400

    Fixed relative access to files outside drive

--- src/br/ufms/facom/student/cloud/server/SimpleDrive.java
+++ src/br/ufms/facom/student/cloud/server/SimpleDrive.java
@@ -3,77 +3,90 @@ package br.ufms.facom.student.cloud.server;
 import br.ufms.facom.student.cloud.rmi.Drive;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.security.DigestInputStream;
-import java.security.DigestOutputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SimpleDrive extends UnicastRemoteObject implements Drive {
-    private File mWorkingDirectory;
-    // private File lockFile;
+    public static final String[] FORBIDDEN_FILENAMES = {".", ".."};
+
+    private final Path mDriveDirectory;
+    // private final File mLockFile;
 
     public SimpleDrive(String path) throws RemoteException {
         super();
-        mWorkingDirectory = new File(path);
+
+        mDriveDirectory = Paths.get(path).toAbsolutePath().normalize();
+        var workingDir = mDriveDirectory.toFile();
 
         // Check the directory existence
-        if (mWorkingDirectory.exists()) {
-            if (!mWorkingDirectory.isDirectory())
+        if (workingDir.exists()) {
+            if (!workingDir.isDirectory())
                 throw new RuntimeException(path+" already exists and it is not a directory");
         } else {
-            mWorkingDirectory.mkdirs();
-            // new File("a/b/c/name.txt").mkdir() -> a/b/c/
-            // new File("a/b/c/name.txt").mkdirs() -> a/b/c/name.txt/
+            workingDir.mkdirs();
         }
     }
 
+    /**
+     * Takes a String and converts it to a path relative to our Working Directory.
+     *
+     * It also ensures path is inside our drive directory and denies the use of relative paths to access private files,
+     * e.g, "../../../some/file". In this case, an exception is thrown.
+     *
+     * @param path String containing path.
+     * @return The path.
+     */
+    public Path convertToLocalPath(String path) throws FileNotFoundException {
+        var filePath = mDriveDirectory.resolve(Paths.get(path)).normalize();
+
+        if (!filePath.startsWith(mDriveDirectory))
+            throw new FileNotFoundException();
+
+        return filePath;
+    }
+
     @Override
-    public void copy(String source, String destination) throws IOException{
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
-        System.out.println("CP "+source+" TO "+destination);
-        var filesource = new File(mWorkingDirectory.getAbsolutePath(), source);
+    public void copy(String source, String destination) throws IOException {
+        System.out.println("COPY "+source+" TO "+destination);
+
+        var filesource = convertToLocalPath(source).toFile();
 
         var filename = filesource.getName();
-        var destinationfolder = new File(mWorkingDirectory.getAbsolutePath(), destination);
+        var destinationfolder = convertToLocalPath(destination).toFile();
         var filedestination = new File(destinationfolder, filename);
 
-        Files.copy(filesource.toPath(),
-                filedestination.toPath(),
-                StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(filesource.toPath(), filedestination.toPath(), StandardCopyOption.REPLACE_EXISTING);
     }
 
     @Override
     public byte[] get(String filename) throws IOException {
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
         System.out.println("GET "+filename);
-        var file = new File(mWorkingDirectory.getAbsolutePath(), filename);
-        var fileInput = new FileInputStream(file);
-        var ret = fileInput.readAllBytes();
-        fileInput.close();
 
-        return ret;
+        var file = convertToLocalPath(filename).toFile();
+        try (var fileInput = new FileInputStream(file)) {
+            return fileInput.readAllBytes();
+        }
     }
 
     @Override
-    public String[] list(String path) throws IOException{
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
-        var fileindirectory = new File(mWorkingDirectory.getAbsolutePath(), path);
-        return fileindirectory.list();
+    public String[] list(String path) throws IOException {
+        return convertToLocalPath(path).toFile().list();
     }
 
     @Override
     public void move(String source, String destination) throws IOException{
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
-        System.out.println("MV "+source+" TO "+destination);
-        var filesource = new File(mWorkingDirectory.getAbsolutePath(), source);
+        System.out.println("MOVE "+source+" TO "+destination);
+        var filesource = convertToLocalPath(source).toFile();
 
         var filename = filesource.getName();
-        var destinationfolder = new File(mWorkingDirectory.getAbsolutePath(), destination);
+        var destinationfolder = convertToLocalPath(destination).toFile();
         var filedestination = new File(destinationfolder, filename);
 
         Files.move(filesource.toPath(),
@@ -83,8 +96,7 @@ public class SimpleDrive extends UnicastRemoteObject implements Drive {
 
     @Override
     public void put(String filename, byte[] data) throws IOException {
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
-        var file = new File(mWorkingDirectory.getAbsolutePath(), filename);
+        var file = convertToLocalPath(filename).toFile();
         var fileoutput = new FileOutputStream(file);
         fileoutput.write(data);
         fileoutput.close();
@@ -92,9 +104,8 @@ public class SimpleDrive extends UnicastRemoteObject implements Drive {
 
     @Override
     public Boolean remove(String filename) throws IOException {
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
         //System.out.println("RM "+filename);
-        var file = new File(mWorkingDirectory.getAbsolutePath(), filename);
+        var file = convertToLocalPath(filename).toFile();
         System.out.println("RM "+file);
         Boolean ret = file.exists();
         if (file.exists()){
@@ -105,8 +116,7 @@ public class SimpleDrive extends UnicastRemoteObject implements Drive {
 
     @Override
     public String hash(String filename) throws IOException, NoSuchAlgorithmException {
-        // FIXME Security flaw: client may use relative paths to access private files, e.g, "../../../some/file".
-        var file = new File(mWorkingDirectory.getAbsolutePath(), filename);
+        var file = convertToLocalPath(filename).toFile();
         var md5 = MessageDigest.getInstance("MD5");
         md5.reset();
 
